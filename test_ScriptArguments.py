3bee53d119ed94d652fcfcc6666c46cae1018c73
martin-a-son <31099355+martin-a-son@users.noreply.github.com>
2017-08-17 18:14:44 +0200

    Update test_ScriptArguments.py
    options fix, some cosmetics

--- test_ScriptArguments.py
+++ test_ScriptArguments.py
@@ -9,9 +9,7 @@ myAuthHash = 'some567hash234='
 
 def usage():
     print ('Usage:')
-	# print ('python myscript.py [-a|--auth <Authentication Hash>] [-h|--help] [-d]')
-    print ('python ' + sys.argv[0] + ' [--ip <Destination IP>] [--auth <Authentication Hash>] [-h|--help] [-d]')
-    print ('-d: debug mode')
+    print ('python ' + sys.argv[0] + ' [-i|--ip <Destination IP>] [-a|--auth <Authentication Hash>] [-h|--help] [-d| --debug]')
 
 def main(argv):
 	# Variables
@@ -21,22 +19,21 @@ def main(argv):
 	_debug = False
 	
 	# Define and get script arguments
+	# Options that require an argument need to be followed by a colon.
+	# I our case we want the options "-h -a <arg1> -i <arg2> -d". That means our option list is "ha:i:d".
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hai:d", ["help", "auth=", "ip="])
+		opts, args = getopt.getopt(sys.argv[1:], "ha:i:d", ["help", "auth=", "ip=", "debug"])
 	except getopt.GetoptError as err:
 		print (err)	# Will print something like "option not recognized".
 		usage()
 		sys.exit(2)
 	
-	# FIXME: 17.08.2017
-	# Weired:
-	# For some reason the options "-a" never works, "-i" sometimes works but "--ip" and "--auth" always work
-	# leaving it for now
+	# Go through the options and pick up their arguments.
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			usage()
 			sys.exit()
-		elif opt == '-d':
+		elif opt in ("-d", "--debug"):
 			_debug = 1
 		elif opt in ("-a", "--auth"):
 			auth = arg
@@ -45,10 +42,15 @@ def main(argv):
 		else:
 			assert False, "unhandled option"
 
+	# Ignore any unwanted arguments.
+	if args:
+		print ('\n***Further arguments ' + str(args) + ' are ignored.***\n')
+		
 	if _debug:
-		print ('myAuthHash = ' + myAuthHash)
+		print ('myAuthHash default = ' + myAuthHash)
 		print ('opts = ' + str(opts))
 		print ('args = ' + str(args))
+		
 		if auth == "":
 			print ('auth = "nix"')
 		else:
@@ -58,5 +60,6 @@ def main(argv):
 			print ('ip = "nix"')
 		else:
 			print ('ip = ' + ip)
+			
 if __name__ == "__main__":
     main(sys.argv[1:])
