69955b3146ef7386a5cab49fc18298d1e7c66638
martin-a-son <31099355+martin-a-son@users.noreply.github.com>
2017-08-17 18:09:03 +0200

    Update test_ScriptArguments.py
    Fixed the issue with the options. Added message that further arguments are ignored.

--- test_ScriptArguments.py
+++ test_ScriptArguments.py
@@ -8,10 +8,8 @@ import getopt
 myAuthHash = 'some567hash234='
 
 def usage():
-    print ('Usage:')
-	# print ('python myscript.py [-a|--auth <Authentication Hash>] [-h|--help] [-d]')
-    print ('python ' + sys.argv[0] + ' [-a|--auth <Authentication Hash>] [-h|--help] [-d]')
-    print ('-d: debug mode')
+	print ('Usage:')
+	print ('python ' + sys.argv[0] + ' [-i|--ip <Destination IP>] [-a|--auth <Authentication Hash>] [-h|--help] [-d| --debug]')
 
 def main(argv):
 	# Variables
@@ -19,30 +17,47 @@ def main(argv):
 	auth = myAuthHash
 	_debug = False
 	
+	# Define and get script arguments
+	# Options that require an argument need to be followed by a colon.
+	# I our case we want the options "-h -a <arg1> -i <arg2> -d". That means our option list is "ha:i:d".
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "ha:d", ["help", "auth="])
+		opts, args = getopt.getopt(sys.argv[1:], "ha:i:d", ["help", "auth=", "ip=", "debug"])
 	except getopt.GetoptError as err:
 		print (err)
 		usage()
 		sys.exit(2)
 	
+	# Go through the options and pick up their arguments.
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			usage()
 			sys.exit()
-		elif opt == '-d':
+		elif opt in ("-d", "--debug"):
 			_debug = 1
 		elif opt in ("-a", "--auth"):
 			auth = arg
+		elif opt in ("-i", "--ip"):
+			ip = arg
 		else:
 			assert False, "unhandled option"
 
+	# Ignore any unwanted arguments.
+	if args:
+		print ('\n***Further arguments ' + str(args) + ' are ignored.***\n')
+
 	if _debug:
 		print ('myAuthHash = ' + myAuthHash)
+		print ('opts = ' + str(opts))
+		print ('args = ' + str(args))
 		if auth == "":
-			print ('auth = "nix"')	# Initially we have "auth = myAuthHash", so "nix" should never be reached.
+			print ('auth = "nix"')
 		else:
 			print ('auth = ' + auth)
+		
+		if ip == "":
+			print ('ip = "nix"')
+		else:
+			print ('ip = ' + ip)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
