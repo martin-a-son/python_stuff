91a5c893c15784c53c1989970a28f751e1b761cd
martin-a-son <31099355+martin-a-son@users.noreply.github.com>
2017-08-17 16:16:01 +0200

    Update test_ScriptArguments.py
    added option for IP
    For some reason only the long options seem to work properly. The short options "-a", "-i" are not reliable.

--- test_ScriptArguments.py
+++ test_ScriptArguments.py
@@ -10,22 +10,28 @@ myAuthHash = 'some567hash234='
 def usage():
     print ('Usage:')
 	# print ('python myscript.py [-a|--auth <Authentication Hash>] [-h|--help] [-d]')
-    print ('python ' + sys.argv[0] + ' [-a|--auth <Authentication Hash>] [-h|--help] [-d]')
+    print ('python ' + sys.argv[0] + ' [--ip <Destination IP>] [--auth <Authentication Hash>] [-h|--help] [-d]')
     print ('-d: debug mode')
 
 def main(argv):
 	# Variables
 	global _debug
 	auth = myAuthHash
+	ip = '127.0.0.1'
 	_debug = False
 	
+	# Define and get script arguments
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "ha:d", ["help", "auth="])
+		opts, args = getopt.getopt(sys.argv[1:], "hai:d", ["help", "auth=", "ip="])
 	except getopt.GetoptError as err:
-		print (err)
+		print (err)	# Will print something like "option not recognized".
 		usage()
 		sys.exit(2)
 	
+	# FIXME: 17.08.2017
+	# Weired:
+	# For some reason the options "-a" never works, "-i" sometimes works but "--ip" and "--auth" always work
+	# leaving it for now
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			usage()
@@ -34,15 +40,23 @@ def main(argv):
 			_debug = 1
 		elif opt in ("-a", "--auth"):
 			auth = arg
+		elif opt in ("-i", "--ip"):
+			ip = arg
 		else:
 			assert False, "unhandled option"
 
 	if _debug:
 		print ('myAuthHash = ' + myAuthHash)
+		print ('opts = ' + str(opts))
+		print ('args = ' + str(args))
 		if auth == "":
-			print ('auth = "nix"')	# Initially we have "auth = myAuthHash", so "nix" should never be reached.
+			print ('auth = "nix"')
 		else:
 			print ('auth = ' + auth)
-
+		
+		if ip == "":
+			print ('ip = "nix"')
+		else:
+			print ('ip = ' + ip)
 if __name__ == "__main__":
     main(sys.argv[1:])
