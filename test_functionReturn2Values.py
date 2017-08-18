7d989487b68bd32d2927be88da2ebca201ca001c
martin-a-son <31099355+martin-a-son@users.noreply.github.com>
2017-08-18 18:07:14 +0200

    Update test_functionReturn2Values.py
    add 3rd return value to function

--- test_functionReturn2Values.py
+++ test_functionReturn2Values.py
@@ -1,21 +1,26 @@
-
+# Call script like below:
+# c:\> python test_functionReturn2Values.py one two
+# argv = ['one', 'two']
+# Wert 1 = value_one
+# Wert 2 = value_two
+# Kombi = onetwo
 
 import sys
 
 def myfunc(strA, strB):
 	x = 'value_' + strA
 	y = 'value_' + strB
-	return x, y
+	z = strA + strB
+	return x, y, z
 	
 def main(argv):
 	print ('argv = ' + str(argv))
 
-	Wert_1, Wert_2 = myfunc(argv[0], argv[1])
+	Wert_1, Wert_2, Kombi = myfunc(argv[0], argv[1])
 	
 	print ('Wert 1 = ' + Wert_1)
 	print ('Wert 2 = ' + Wert_2)
-
-
+	print ('Kombi = ' + Kombi)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
